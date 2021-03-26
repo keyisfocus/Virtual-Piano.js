fd116ce6a7fc52bcd441a1ce56d7361f7d654903
keyisfocus <keyisfocus@gmail.com>
2021-03-26 18:04:29 +0300

    Stage 4

--- script.js
+++ script.js
@@ -1,25 +1,40 @@
 document.addEventListener("keypress", function (e) {
+    let audio;
     switch (e.code) {
         case "KeyA":
             console.log("The 'A' key is pressed.");
+            audio = new Audio("A.mp3");
+            audio.play();
             break;
         case "KeyS":
             console.log("The 'S' key is pressed.");
+            audio = new Audio("S.mp3");
+            audio.play();
             break;
         case "KeyD":
             console.log("The 'D' key is pressed.");
+            audio = new Audio("D.mp3");
+            audio.play();
             break;
         case "KeyF":
             console.log("The 'F' key is pressed.");
+            audio = new Audio("F.mp3");
+            audio.play();
             break;
         case "KeyG":
             console.log("The 'G' key is pressed.");
+            audio = new Audio("G.mp3");
+            audio.play();
             break;
         case "KeyH":
             console.log("The 'H' key is pressed.");
+            audio = new Audio("H.mp3");
+            audio.play();
             break;
         case "KeyJ":
             console.log("The 'J' key is pressed.");
+            audio = new Audio("J.mp3");
+            audio.play();
             break;
         default:
             console.log("Wrong key");
